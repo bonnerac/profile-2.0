c21fc98fc652d0ef1d1bdb73cb9db306e3108939
bonnerac <alex.c.bonner@gmail.com>
2020-02-08 20:05:05 -0600

    changed landing page to boombox which shakes and has a tape playing on click

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 window.onload = function () {
     window.scrollTo(1368, 0);
 }
+
 $("body").css("overflow", "hidden");
 
-$("#button").click(function () {
-    $('html, body').animate({
+$("#play").on("click", function () {
+    $("#tape").attr("src", "./tape giphy.gif")
+    $("#tape").css("height", "200")
+    $("#tape").css("width", "320")
+    $("#tape").css("padding", "10")
+    $("#boombox").css("animation", "shake 1s")
+    $("#boombox").css("animation-iteration-count", "infinite")
+    $("#tape").css("animation", "shake 1s")
+    $("#tape").css("animation-iteration-count", "infinite")
+    $("#play").css("animation", "shake 1s")
+    $("#play").css("animation-iteration-count", "infinite")
+    $("#name").css("top", "0%")
+    $("#name").css("font-size", "70px")
+    $("#name").css("left", "133%")
+    $("#name").css("color", "white")
+})
+
+$("#play").click(function () {
+    $('html, body').delay(2000).animate({
         scrollTop: 802
     }, 2000);
 })
